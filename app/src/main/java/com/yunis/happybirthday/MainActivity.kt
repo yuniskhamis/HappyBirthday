f2eb9a3134662326872697266892a9da9d8ef87e
yuniskhamis <yuniskhamis3@gmail.com>
2021-07-17 20:15:27 -0400

    Initial commit

---
+++ app/src/main/java/com/yunis/happybirthday/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.yunis.happybirthday
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * Displays a birthday message.
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
